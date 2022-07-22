671f31de3472e31ff30bc7b064a3c5eebe0850eb
Pe-ngo <jamespengo95@gmail.com>
2022-07-22 14:25:58 +0300

    Completed assignment

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,7 +2,20 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
 /* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
+export function usePokemon(query) {
+  const [data, setData] = useState(null)
+
+  useEffect(()=>{
+    fetch(query)
+    .then(res=>res.json())
+    .then(data=>{
+      setData(data)
+    })
+  },[])
+  
+  return {
+    data
+  }
   /* ✅ this hook should only return one thing: an object with the pokemon data */
 }
 
@@ -11,12 +24,14 @@ function Pokemon({ query }) {
    ✅ move the code from the useState and useEffect hooks into the usePokemon hook
    then, call the usePokemon hook to access the pokemon data in this component
   */
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then(r => r.json())
-      .then(setPokemon);
-  }, [query]);
+  // const [pokemon, setPokemon] = useState(null);
+  // useEffect(() => {
+  //   fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+  //     .then(r => r.json())
+  //     .then(setPokemon);
+  // }, [query]);
+
+  const {data:pokemon} = usePokemon(`https://pokeapi.co/api/v2/pokemon/${query}`)
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
   if (!pokemon) return <h3>Loading...</h3>;
@@ -60,7 +75,6 @@ const Wrapper = styled.section`
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   background: papayawhip;
   text-align: center;
-
   h1 {
     background: #ef5350;
     color: white;
@@ -70,7 +84,6 @@ const Wrapper = styled.section`
     color: white;
     font-size: 2rem;
   }
-
   form {
     display: grid;
     grid-template-columns: 1fr auto;
